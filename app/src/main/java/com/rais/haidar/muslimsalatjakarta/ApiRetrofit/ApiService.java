e7ce94919d85daf0c9b5c5d3b199e3206a921194
HaidarRais <zwingfame@gmail.com>
2018-08-27 11:35:44 +0700

    mulai Networking

---
+++ app/src/main/java/com/rais/haidar/muslimsalatjakarta/ApiRetrofit/ApiService.java
@@ -0,0 +1,8 @@
+package com.rais.haidar.muslimsalatjakarta.ApiRetrofit;
+
+/**
+ * Created by Hoidar on 8/27/18.
+ */
+
+public interface ApiService {
+}
---
+++ app/src/main/java/com/rais/haidar/muslimsalatjakarta/ApiRetrofit/InstanceRetrofit.java
@@ -0,0 +1,10 @@
+package com.rais.haidar.muslimsalatjakarta.ApiRetrofit;
+
+import retrofit2.http.GET;
+
+/**
+ * Created by Hoidar on 8/27/18.
+ */
+
+public class InstanceRetrofit {
+}
